5f3de9f25eb58c92d877fc6288fede5d8dc8468b
amartripathi0 <theamartripathi@gmail.com>
2024-10-17 18:38:27 +0530

    add academic detail form validations

--- client/src/utils/formValidation.js
+++ client/src/utils/formValidation.js
@@ -1,77 +1,105 @@
-
 export const firstNameValidation = {
-    required: {
-      value: true,
-      message: "Please enter your first name.",
-    },
-  };
-  
-  export const lastNameValidation = {
-    required: {
-      value: true,
-      message: "Please enter your last name.",
-    },
-  };
-  export const fathersNameValidation = {
-    required: {
-      value: true,
-      message: "Please enter your father's name.",
-    },
-  };
-  export const mothersNameValidation = {
-    required: {
-      value: true,
-      message: "Please enter your mother's name.",
-    },
-  };
-  
-  export const emailValidation = {
-    required: {
-      value: true,
-      message: "Please enter your email address.",
-    },
-    pattern: {
-      value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
-      message: "Please enter a valid Email address",
-    },
-  };
-  
-  export const phoneValidation = {
-    required: {
-      value: true,
-      message: "Please enter your Phone No.",
-    },
-    maxLength: {
-      value: 10,
-      message: "Please Enter Phone Number under 10 digits",
-    },
-    minLength: {
-      value: 10,
-      message: "Please Enter Phone Number under 10 digits",
-    },
-  };
-  
-  export const passwordValidation = {
-    required: {
-      value: true,
-      message: "Please enter your Password.",
-    },
-    minLength: {
-      value: 6,
-      message: "Please enter a strong password of length more than 5.",
-    },
-  };
-  
-  export const staffIdValidation = {
-    required : {
-      value : true ,
-      message : "Please enter your Staff-ID"
-    },
-    
-  }
-  export const companyNameValidation = {
-    required: {
-      value: true,
-      message: "Please enter your company name.",
-    },
-  };
+  required: {
+    value: true,
+    message: "Please enter your first name.",
+  },
+};
+
+export const lastNameValidation = {
+  required: {
+    value: true,
+    message: "Please enter your last name.",
+  },
+};
+export const fathersNameValidation = {
+  required: {
+    value: true,
+    message: "Please enter your father's name.",
+  },
+};
+export const mothersNameValidation = {
+  required: {
+    value: true,
+    message: "Please enter your mother's name.",
+  },
+};
+export const rollNumberValidation = {
+  required: {
+    value: true,
+    message: "Please enter the Roll Number.",
+  },
+};
+export const degreeValidation = {
+  required: {
+    value: true,
+    message: "Please enter your Degrees",
+  },
+};
+export const collegeNameValidation = {
+  required: {
+    value: true,
+    message: "Please enter your College Name",
+  },
+};
+export const cgpaValidation = {
+  required: {
+    value: true,
+    message: "Please enter your CGPA",
+  },
+};
+export const yearOfPassingValidation = {
+  required: {
+    value: true,
+    message: "Please enter your Year of Passing",
+  },
+};
+
+export const emailValidation = {
+  required: {
+    value: true,
+    message: "Please enter your email address.",
+  },
+  pattern: {
+    value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+    message: "Please enter a valid Email address",
+  },
+};
+
+export const phoneValidation = {
+  required: {
+    value: true,
+    message: "Please enter your Phone No.",
+  },
+  maxLength: {
+    value: 10,
+    message: "Please Enter Phone Number under 10 digits",
+  },
+  minLength: {
+    value: 10,
+    message: "Please Enter Phone Number under 10 digits",
+  },
+};
+
+export const passwordValidation = {
+  required: {
+    value: true,
+    message: "Please enter your Password.",
+  },
+  minLength: {
+    value: 6,
+    message: "Please enter a strong password of length more than 5.",
+  },
+};
+
+export const staffIdValidation = {
+  required: {
+    value: true,
+    message: "Please enter your Staff-ID",
+  },
+};
+export const companyNameValidation = {
+  required: {
+    value: true,
+    message: "Please enter your company name.",
+  },
+};
