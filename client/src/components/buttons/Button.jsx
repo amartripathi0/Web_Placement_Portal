f0c28e8245840cad831ddac20b7076d1416ce90b
amartripathi0 <theamartripathi@gmail.com>
2024-10-17 15:56:46 +0530

    fix: button styles

--- client/src/components/buttons/Button.jsx
+++ client/src/components/buttons/Button.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({label , onClickHandler , additionalStyles , type , color}) => {
+const Button = ({ label, onClickHandler, additionalStyles, type, color }) => {
   // console.log(onClickHandler);
   return (
-    
-    <button onClick={onClickHandler} type = {type} className={` ${additionalStyles}  text-md text-white  ${color === "pink" ?  "bg-pink-500 hover:bg-pink-600" : "bg-cyan-500 hover:bg-cyan-600  "} py-2 px-3 rounded-lg flex-center transition-colors duration-200`}>
-
-        {label}
-        
-    
+    <button
+      onClick={onClickHandler}
+      type={type}
+      className={` ${additionalStyles}  text-sm text-white  ${
+        color === "pink"
+          ? "bg-pink-500 hover:bg-pink-600"
+          : "bg-cyan-500 hover:bg-cyan-600  "
+      } py-2 px-3 rounded flex-center transition-colors duration-200 font-medium`}
+    >
+      {label}
     </button>
-    
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
