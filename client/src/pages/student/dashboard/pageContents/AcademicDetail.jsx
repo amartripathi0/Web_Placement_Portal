b93b8433efb5c5eddaca08ee55885851b5fa797a
amartripathi0 <theamartripathi@gmail.com>
2024-10-17 18:40:03 +0530

    fix: academic detail page layout and style

--- client/src/pages/student/dashboard/pageContents/AcademicDetail.jsx
+++ client/src/pages/student/dashboard/pageContents/AcademicDetail.jsx
@@ -13,6 +13,18 @@ import {
   getUserData,
   updateProfileDetail,
 } from "../../../../redux/features/student/auth/authSlice";
+import AccountStatus from "../../../../components/AccountStatus";
+import FormField from "../../../../components/containers/FormField";
+import Label from "../../../../components/label";
+import {
+  cgpaValidation,
+  collegeNameValidation,
+  degreeValidation,
+  firstNameValidation,
+  rollNumberValidation,
+  yearOfPassingValidation,
+} from "../../../../utils/formValidation";
+import Button from "../../../../components/buttons/Button";
 
 const AcademicDetail = () => {
   const dispatch = useDispatch();
@@ -58,125 +70,89 @@ const AcademicDetail = () => {
   // console.log(student);
   return (
     <div
-      className={`h-full bg-purple-100  flex justify-center items-center ${
+      className={`h-screen bg-purple-100  flex justify-center items-center p-10 pt-32 ${
         (isLoading || studentUtil.isLoading) && " bg-gray-400 blur-sm"
       }`}
     >
-      <form
-        action=""
-        onSubmit={handleSubmit(formSubmit)}
-        className="bg-slate-100  w-[85%] h-[85%] rounded-lg shadow-slate-300 shadow-md flex justify-center items-center"
-        noValidate
+      <div
+        
+        className="bg-slate-100 h-full w-full rounded-lg shadow-slate-300 shadow-md flex justify-center items-center p-6 px-20"
+       
       >
-        <div className="flex flex-col p-20 gap-3 bg-white rounded-md w-[85%] shadow-grey-300 shadow-md">
-          <div
-            className={`${
-              student?.role === "Allowed"
-                ? " bg-green-500 shadow-green-300 shadow-md"
-                : " bg-red-600 shadow-red-500 shadow-md"
-            }
-        p-2 w-40 flex items-center justify-center rounded-md mb-10 ml-6 `}
-          >
-            <h1 className="text-white text-2xl font-semibold">
-              {" "}
-              {student?.role}
-            </h1>
-          </div>
+        <form action="" noValidate
+        onSubmit={handleSubmit(formSubmit)} className="flex flex-col gap-4 bg-white rounded-md h-full w-full shadow-grey-300 shadow-md border p-8">
+          <AccountStatus role={student?.role} />
 
-          <div className="flex items-center gap-56  justify-between  w-3/5 py-3 px-6 rounded-md  hover:bg-purple-100 ">
-            <h2 className="text-xl">Roll Number</h2>
+          <FormField>
+            <Label>Roll Number</Label>
             <InputWithEdit
               type="number"
               placeholder="Roll Number"
               validationObj={{
-                ...register("rollNumber", {
-                  required: {
-                    value: true,
-                    message: "Please enter the Roll Number.",
-                  },
-                  validate: (v) => v > 0 || "Please enter a valid Roll number.",
-                }),
+                ...register("rollNumber", rollNumberValidation),
+                validate: (v) => v > 0 || "Please enter a valid Roll number.",
               }}
               error={errors.rollNumber?.message}
             />
-          </div>
-
-          <div className="flex items-center justify-between  gap-56   w-3/5 py-3 px-6 rounded-md  hover:bg-purple-100">
-            <h2 className="text-xl">Degree</h2>
+          </FormField>
+          <FormField>
+            <Label>Degrees</Label>
             <InputWithEdit
-              type="select"
+              type="text"
               placeholder="Degree"
               validationObj={{
-                ...register("degrees", {
-                  required: {
-                    value: true,
-                    message: "Please enter your Degrees",
-                  },
-                }),
+                ...register("degrees", degreeValidation),
               }}
               error={errors.degrees?.message}
             />
-          </div>
-
-          <div className="flex items-center gap-56  justify-between  w-3/5 py-3 px-6 rounded-md  hover:bg-purple-100 ">
-            <h2 className="text-xl">College</h2>
+          </FormField>
+          <FormField>
+            <Label>College</Label>
             <InputWithEdit
               type="text"
-              placeholder="College"
+              placeholder="College Name"
               validationObj={{
-                ...register("collegeName", {
-                  required: {
-                    value: true,
-                    message: "Please enter the College Name.",
-                  },
-                }),
+                ...register("collegeName", collegeNameValidation),
               }}
               error={errors.collegeName?.message}
             />
-          </div>
+          </FormField>
 
-          <div className="flex items-center gap-56  justify-between  w-3/5 py-3 px-6 rounded-md  hover:bg-purple-100 ">
-            <h2 className="text-xl">CGPA</h2>
+          <FormField>
+            <Label>CGPA</Label>
             <InputWithEdit
               type="number"
               placeholder="CGPA"
               validationObj={{
-                ...register("cgpa", {
-                  required: {
-                    value: true,
-                    message: "Please enter the latest CGPA",
-                  },
-                }),
+                ...register("cgpa", cgpaValidation),
+                validate: (v) => v > 0 || "Please enter a valid CGPA.",
               }}
               error={errors.cgpa?.message}
             />
-          </div>
+          </FormField>
 
-          <div className="flex items-center gap-56  justify-between  w-3/5 py-3 px-6 rounded-md  hover:bg-purple-100 ">
-            <h2 className="text-xl"> Year Of Passing</h2>
+          <FormField>
+            <Label>Year Of Passing</Label>
             <InputWithEdit
               type="text"
               placeholder="Year Of Passing"
               validationObj={{
-                ...register("yearOfPassing", {
-                  required: {
-                    value: true,
-                    message: "Please enter the Year of Passing",
-                  },
-                }),
+                ...register("yearOfPassing", yearOfPassingValidation),
               }}
               error={errors.yearOfPassing?.message}
             />
-          </div>
+          </FormField>
 
-          <button
-            type="submit"
-            className="w-60 mx-5 mt-20 font-semibold text-white bg-pink-500 hover:bg-pink-600 p-3 pl-6 pr-6 rounded-lg flex items-center justify-center"
-          >
-            Save
-          </button>
-        </div>
-      </form>
+          <Button
+            type={"submit"}
+            color={"pink"}
+            label={"Save Details"}
+            additionalStyles={
+              "w-40 mt-4 mx-auto max-sm:w-10/12 max-sm:text-xs max-sm:p-3  "
+            }
+          />
+        </form>
+      </div>
     </div>
   );
 };
