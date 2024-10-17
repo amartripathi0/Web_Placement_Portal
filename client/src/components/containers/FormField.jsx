22bb3d8f26fbec85a28e9b94bd914dff4343e50f
amartripathi0 <theamartripathi@gmail.com>
2024-10-17 15:40:08 +0530

    fix: UI fixes in InputWithEdit and FormField

--- client/src/components/containers/FormField.jsx
+++ client/src/components/containers/FormField.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const FormField = ({children , additionalStyles}) => {
+const FormField = ({ children, additionalStyles }) => {
   return (
     <div
-    className={` ${additionalStyles} flex justify-between sm:gap-4 w-11/12   rounded-lg  px-6 py-1  h-[4.5vw] max-tablet:h-[8vw]
-    max-sm:flex-col max-sm:h-32 max-sm:p-2 max-sm:gap-2 
-    border-2 border-gray-200 hover:bg-gray-50 transition-colors duration-100
-     
+      className={` ${additionalStyles} flex justify-between sm:gap-4 w-11/12 rounded  px-2.5 py-1.5 h-16 max-tablet:h-[8vw] max-sm:flex-col max-sm:h-32 max-sm:p-2 max-sm:gap-2 
+      border border-gray-200 hover:bg-gray-50 transition-colors duration-100
     `}
     >
-        {children}
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default FormField
+export default FormField;
--- client/src/components/inputField/InputWithEdit.jsx
+++ client/src/components/inputField/InputWithEdit.jsx
@@ -12,31 +12,47 @@ const InputWithEdit = ({
     setShowEdit(prev => !prev)
   }
   return (
-    <div className={`flex flex-col gap-1 h-full  ${inputFieldContainerStyles}`}>
+    <div className={`flex flex-col gap-1 ${inputFieldContainerStyles}`}>
+      <div
+        className={` ${
+          showEdit && "border border-neutral-700"
+        } w-full h-full flex justify-between items-center`}
+      >
+        <input
+          type={type}
+          readOnly={!showEdit ? true : false}
+          placeholder={placeholder}
+          className={`h-full px-2 w-[90%] font-medium text-base text-right  ${
+            !showEdit && " outline-none"
+          } ${inputFieldStyle} `}
+          {...validationObj}
+          onClick={() =>
+            !showEdit &&
+            toast.warn("Please click on the edit button to edit the fields.", {
+              position: toast.POSITION.TOP_RIGHT,
+            })
+          }
+          value={value}
+        />
 
-    <div className= {`border-black ${showEdit && "border-2"} h-3/5 w-full text-md rounded-md  flex justify-between items-center`}>
-      <input
-      type= {type}
-      readOnly = {!showEdit ? true : false}
-      placeholder={placeholder}
-      className={`h-full pl-3 w-[90%] font-medium text-[1vw] text-right pr-4  ${!showEdit && " outline-none"} ${inputFieldStyle} `}
-      {...validationObj}
-      onClick={() => !showEdit && toast.warn("Please click on the edit button to edit the fields.",{position : toast.POSITION.TOP_RIGHT})}
-      value={value}
-      />
-      
-      <div onClick={toggleEdit} className={`${showEdit &&  "border-2 border-l-black"} border-r-white border-y-white h-full pl-2 px-1 flex justify-center items-center`}>
-        {
-          showEdit ?
-          <FaSave size={20}  />
-          :
-        <MdModeEdit size={20} />
-        }
+        <div
+          onClick={toggleEdit}
+          className={`${
+            showEdit && "border border-l-neutral-700"
+          } border-r-white border-y-white h-full pl-2 px-1 flex justify-center items-center`}
+        >
+          {showEdit ? <FaSave size={20} /> : <MdModeEdit size={20} />}
+        </div>
       </div>
-    </div>
-        
-    <p className={` ${errorMessageStyle  } ${ !error && "hidden"} h-2/5 font-medium text-[0.9vw] text-red-500  text-right`}> {error}</p>
-      
+
+      <p
+        className={` ${errorMessageStyle} ${
+          !error && "invisible"
+        } h-2/5 font-medium text-xs text-red-500 text-right`}
+      >
+        {" "}
+        {error}
+      </p>
     </div>
   );
 };
