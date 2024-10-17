f4dc59260975dc353acb0707bb6ec952d5790eeb
amartripathi0 <theamartripathi@gmail.com>
2024-10-17 18:38:56 +0530

    fix: account status and label comp UI

--- client/src/components/AccountStatus.jsx
+++ client/src/components/AccountStatus.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const AccountStatus = ({role}) => {
+const AccountStatus = ({ role }) => {
   return (
     <div
-    className={`${
-      role === "Allowed"
-        ? " bg-green-500 shadow-green-300 shadow-sm"
-        : " bg-red-600 shadow-red-500 shadow-sm"
-    }
-    p-2 w-2/5 flex-center rounded hover:shadow-md transition-shadow`}
-  >
-    <h1 className="text-white text-sm max-tablet:text-base max-sm:text-medium font-semibold">
+      className={`${
+        role === "Allowed"
+          ? " bg-green-500 shadow-green-300 shadow-sm"
+          : " bg-red-600 shadow-red-500 shadow-sm"
+      }
+    p-2 w-28 flex-center rounded hover:shadow-md transition-shadow text-white text-sm max-tablet:text-base max-sm:text-medium font-semibold`}
+    >
       {role}
-    </h1>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default AccountStatus
+export default AccountStatus;
---
+++ client/src/components/label.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Label({ children }) {
+  return <label className="text-sm">{children}</label>;
+}
