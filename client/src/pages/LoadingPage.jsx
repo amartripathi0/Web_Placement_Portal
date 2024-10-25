b863268a50f66a93c0b84b2df42396f9451729d8
amartripathi0 <theamartripathi@gmail.com>
2024-10-25 18:01:37 +0530

    fix(UI): loading oage

--- client/src/pages/LoadingPage.jsx
+++ client/src/pages/LoadingPage.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import {DotLoader, SyncLoader} from 'react-spinners'
-const LoadingPage = ({height = "screen" , width = "screen"}) => {
-
-
+import React from "react";
+import { ClipLoader } from "react-spinners";
+import cn from "../utils/cn";
+const LoadingPage = ({ height = "screen", width = "screen" }) => {
   return (
-    <div className={`h-${height} w-${width} absolute z-50 opacity-100 blur-0 flex justify-center items-center ` }>
-
-        <SyncLoader color="purple" />
+    <div
+      className={cn(
+        `h-full w-full absolute z-50 opacity-100 flex gap-2 justify-center items-center backdrop-blur-sm`,
+        height,
+        width
+      )}
+    >
+      <p className="text-xl font-medium text-fuchsia-900 ">Loading</p>
+      <ClipLoader color="purple" size={22} />
     </div>
-  )
-}
+  );
+};
 
-export default LoadingPage 
+export default LoadingPage;
