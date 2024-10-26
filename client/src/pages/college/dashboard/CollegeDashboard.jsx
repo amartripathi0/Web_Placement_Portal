86565794063d863a612be1681f3e5068d69dca36
amartripathi0 <theamartripathi@gmail.com>
2024-10-26 17:02:10 +0530

    fix(UI): college dashboard page

--- client/src/pages/college/dashboard/CollegeDashboard.jsx
+++ client/src/pages/college/dashboard/CollegeDashboard.jsx
@@ -7,105 +7,91 @@ import { useDispatch, useSelector } from "react-redux";
 import { createContext } from "react";
 
 import {
-  RESET_GLOBAL,
-  SET_GLOBAL,
   getLoginStatus,
+  RESET_GLOBAL,
 } from "../../../redux/features/common/globalSlice";
 import { CollegeDashHeader } from "./CollegeDashHeader";
-import {
-  RESET_COLLEGE_UTIL,
-  getCollegeStaffData,
-} from "../../../redux/features/college/utilServices/collegeUtilSlice";
+import { getCollegeStaffData } from "../../../redux/features/college/utilServices/collegeUtilSlice";
 import { collegeStaffSidebarItems } from "../../../constants";
+import cn from "../../../utils/cn";
 
 const CollegeDashboard = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [sidemenuExpanded, setSidemenuExpanded] = useState(true);
+  const [apiURL, setAPIURL] = useState("");
 
   const globalAuth = useSelector((state) => state.globalAuth);
   const { isLoggedIn, isLoading, isSuccess, collegeStaff, message } =
     useSelector((state) => state.collegeStaffUtil);
+
   useEffect(() => {
-    if (
-      isLoggedIn &&
-      isSuccess &&
-      globalAuth.isLoggedin &&
-      globalAuth.userType === "college-staff" &&
-      collegeStaff.role !== "suspended"
-    ) {
-      toast.success(
-        `Welcome, ${
-          collegeStaff.personalDetail.firstName +
-          " " +
-          collegeStaff.personalDetail.lastName
-        }`,
-        {
-          position: toast.POSITION.TOP_RIGHT,
-        }
-      );
-    } else if (globalAuth.isLoggedin && globalAuth.isSuccess) {
+    if (globalAuth.isLoggedin && globalAuth.isSuccess) {
       dispatch(getCollegeStaffData());
     } else if (
       !globalAuth.isLoggedin &&
       globalAuth.isSuccess &&
       globalAuth.userType !== "college-staff"
     ) {
-      toast.info("Signed Out successfully", {
+      toast.error("Unauthorized Access! Please SignIn", {
         position: toast.POSITION.TOP_RIGHT,
       });
-      // dispatch(SET_GLOBAL("college-staff"));
       navigate("/signin");
+      dispatch(RESET_GLOBAL);
     } else {
       dispatch(getLoginStatus());
     }
   }, [getLoginStatus.isLoggedin, globalAuth.isSuccess, globalAuth.userType]);
 
-  const [sidemenuExpanded, setSidemenuExpanded] = useState(true);
   function sidemenuState(val) {
     setSidemenuExpanded(!val);
   }
 
-  const [apiURL, setAPIURL] = useState("");
   return (
-    <div>
-      {collegeStaff?.role && collegeStaff.role === "Suspended" ? (
+    <main>
+      {collegeStaff?.role === "Suspended" ? (
         <div className="h-screen w-screen  text-6xl flex justify-center items-center bg-slate-100 absolute z-50 top-0 left-0">
           You are Suspended, Contact Admin{" "}
         </div>
       ) : (
-        <div
-          className={`relative flex w-screen h-screen  ${
-            isLoading && " opacity-50 "
-          }`}
-        >
-          {isLoading && <LoadingPage height="screen" width="screen" />}
-
-          <Sidemenu
+        <>
+          {isLoading && <LoadingPage />}
+          <div className={`relative flex`}>
+            <Sidemenu
               sidemenuState={sidemenuState}
               emailID={collegeStaff?.personalDetail.emailID}
               firstName={collegeStaff?.personalDetail.firstName}
               lastName={collegeStaff?.personalDetail.lastName}
               profileImgLink={collegeStaff?.personalDetail.profilePicture}
-            >
-              {collegeStaffSidebarItems.map(item => 
-                 (
-                  <SidebarItem key={item.label} text={item.label} icon={item.icon} active  />
-                ))}
-            </Sidemenu>
+              sidebarItems={collegeStaffSidebarItems}
+            />
 
-          <div
-             className={`flex flex-col  absolute  overflow-x-hidden right-0 top-0 min-h-screen 
-             max-tablet:w-[92%] max-sm:w-[84%] 
-            ${sidemenuExpanded ? "w-[85%]" : "w-[95%] "} `}
-          >
-            <apiContext.Provider value={{ apiURL, setAPIURL }}>
-              <CollegeDashHeader  sidemenuExpanded = {sidemenuExpanded}/>
-              <Outlet />
-            </apiContext.Provider>
+            <section
+              className={cn(
+                `flex flex-col absolute overflow-x-hidden right-0 min-h-screen max-tablet:w-[92%] max-sm:w-[84%] `,
+                sidemenuExpanded ? "w-[85%]" : "w-[95%]"
+              )}
+            >
+              <apiContext.Provider value={{ apiURL, setAPIURL }}>
+                <CollegeDashHeader
+                  sidemenuExpanded={sidemenuExpanded}
+                  userName={
+                    collegeStaff?.personalDetail?.firstName +
+                    " " +
+                    collegeStaff?.personalDetail?.lastName
+                  }
+                  notficationCount={collegeStaff?.notifications.length}
+                  notficationFrom={collegeStaff?.notifications.from}
+                  notficationTitle={collegeStaff?.notifications.title}
+                  notificationBody={collegeStaff?.notifications.body}
+                />
+                <Outlet />
+              </apiContext.Provider>
+            </section>
           </div>
-        </div>
+        </>
       )}
-    </div>
+    </main>
   );
 };
 export const apiContext = createContext();
