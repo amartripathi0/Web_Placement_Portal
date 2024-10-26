5d242739bddfb3064e7875892d63d3b5fbc67efb
amartripathi0 <theamartripathi@gmail.com>
2024-10-26 11:42:35 +0530

    fix(UI): reset password page

--- client/src/pages/student/dashboard/pageContents/ResetPassword.jsx
+++ client/src/pages/student/dashboard/pageContents/ResetPassword.jsx
@@ -3,12 +3,13 @@ import { useForm } from "react-hook-form";
 import PasswordInput from "../../../../components/inputField/PasswordInput";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import UserLayout from "../../../../components/layout/UserLayout";
 import {
   signin,
   RESET,
 } from "../../../../redux/features/student/auth/authSlice";
-import LoadingPage from "../../../LoadingPage";
 import "react-toastify/dist/ReactToastify.css";
+import Button from "../../../../components/buttons/Button";
 
 const ResetPassword = () => {
   const dispatch = useDispatch();
@@ -32,94 +33,83 @@ const ResetPassword = () => {
     // console.log(data);
   }
   return (
-    <div
-      className={`h-screen bg-purple-100  flex justify-center items-center ${
-        isLoading && " opacity-70 bg-gray-400"
-      }`}
-    >
-      {isLoading && <LoadingPage height="screen" width="screen" />}
+    <UserLayout slateBgStyles={"p-14 w-2/5"}>
+      <form
+        action=""
+        onSubmit={handleSubmit(handleResetData)}
+        noValidate
+        className="flex flex-col p-8 gap-6  bg-white rounded-md shadow-grey-300  shadow-md"
+      >
+        <h3 className="text-xl text-center font-medium ">
+          Reset Password
+        </h3>
 
-      <div className="bg-slate-100  w-[85%] h-[85%] rounded-lg shadow-slate-300 shadow-md flex flex-col gap-10 justify-center items-center">
-        <form
-          action=""
-          onSubmit={handleSubmit(handleResetData)}
-          noValidate
-          className="flex flex-col px-14 py-20 gap-12 bg-white rounded-md shadow-grey-300  shadow-md"
-        >
-          <h1 className="text-3xl text-center font-medium ">
-            Reset Password Form
-          </h1>
-
-          <div className="flex flex-col gap-4">
-            <PasswordInput
-              placeholder="Enter your Current Password"
-              label="Current Password"
-              labelName="currentPassword"
-              validationObj={{
-                ...register("currentPassword", {
-                  required: {
-                    value: true,
-                    message: "Please enter your Current Password.",
-                  },
-                  minLength: {
-                    value: 6,
-                    message:
-                      "Please enter a strong password of length more than 5.",
-                  },
-                }),
-              }}
-              error={errors.currentPassword?.message}
-            />
-            <PasswordInput
-              placeholder="Enter your New Password"
-              label="New Password"
-              labelName="newPassword"
-              validationObj={{
-                ...register("newPassword", {
-                  required: {
-                    value: true,
-                    message: "Please enter your New Password.",
-                  },
-                  minLength: {
-                    value: 6,
-                    message:
-                      "Please enter a strong password of length more than 5.",
-                  },
-                }),
-              }}
-              error={errors.newPassword?.message}
-            />
-            <PasswordInput
-              placeholder="Enter your Confirm New Password"
-              label="Comfirm New Password"
-              labelName="confirmNewPassword"
-              validationObj={{
-                ...register("confirmNewPassword", {
-                  required: {
-                    value: true,
-                    message: "Please enter your Confirm New Password.",
-                  },
-                  minLength: {
-                    value: 6,
-                    message:
-                      "Please enter a strong password of length more than 5.",
-                  },
-                }),
-              }}
-              error={errors.confirmNewPassword?.message}
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <button
-              type="submit"
-              className="border-2 bg-blue-400 border-black w-full text-xl p-3 pt-1 pb-1 mt-1 rounded-lg"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+        <div className="flex flex-col gap-4">
+          <PasswordInput
+            placeholder="Enter your Current Password"
+            label="Current Password"
+            labelName="currentPassword"
+            validationObj={{
+              ...register("currentPassword", {
+                required: {
+                  value: true,
+                  message: "Please enter your Current Password.",
+                },
+                minLength: {
+                  value: 6,
+                  message:
+                    "Please enter a strong password of length more than 5.",
+                },
+              }),
+            }}
+            error={errors.currentPassword?.message}
+          />
+          <PasswordInput
+            placeholder="Enter your New Password"
+            label="New Password"
+            labelName="newPassword"
+            validationObj={{
+              ...register("newPassword", {
+                required: {
+                  value: true,
+                  message: "Please enter your New Password.",
+                },
+                minLength: {
+                  value: 6,
+                  message:
+                    "Please enter a strong password of length more than 5.",
+                },
+              }),
+            }}
+            error={errors.newPassword?.message}
+          />
+          <PasswordInput
+            placeholder="Enter your Confirm New Password"
+            label="Comfirm New Password"
+            labelName="confirmNewPassword"
+            validationObj={{
+              ...register("confirmNewPassword", {
+                required: {
+                  value: true,
+                  message: "Please enter your Confirm New Password.",
+                },
+                minLength: {
+                  value: 6,
+                  message:
+                    "Please enter a strong password of length more than 5.",
+                },
+              }),
+            }}
+            error={errors.confirmNewPassword?.message}
+          />
+        </div>
+        <Button
+        label={"Submit"}
+        type={"submit"}
+        additionalStyles={"bg-blue-500"}
+        />
+      </form>
+    </UserLayout>
   );
 };
 
