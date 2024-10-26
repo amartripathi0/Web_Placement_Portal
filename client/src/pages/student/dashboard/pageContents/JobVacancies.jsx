2b9068b7a80080d37f5315753a831af66cb2d14e
amartripathi0 <theamartripathi@gmail.com>
2024-10-26 14:22:18 +0530

    fix: job vacancies pag

--- client/src/pages/student/dashboard/pageContents/JobVacancies.jsx
+++ client/src/pages/student/dashboard/pageContents/JobVacancies.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from "react";
-import InputWithEdit from "../../../../components/inputField/InputWithEdit";
-import { useForm } from "react-hook-form";
-import { toast } from "react-toastify";
 import { useDispatch, useSelector } from "react-redux";
-import { nanoid } from "nanoid";
-import { NavLink } from "react-router-dom";
 import {
   getJobs,
   jobApplyByStudent,
@@ -13,33 +8,32 @@ import JobCard from "../../../../components/job-card";
 import UserLayout from "../../../../components/layout/UserLayout";
 
 const JobVacancies = () => {
-  const { isLoggedIn, isError, isSuccess, isLoading, data, message } =
-    useSelector((state) => state.studentUtils);
   const dispatch = useDispatch();
+  const { isSuccess, isLoading, data, message } = useSelector(
+    (state) => state.studentUtils
+  );
 
   useEffect(() => {
     if (isSuccess && data) {
-      // console.log(data);
     } else {
       dispatch(getJobs());
     }
   }, [isSuccess, data, dispatch, message]);
 
-  // console.log(message);
-  // if(message) {
-  //   toast.success(message , {
-  //     position : toast.POSITION.TOP_RIGHT
-  //   })
-  // }
-  function handleJobApply(jobID) {
-    // console.log(jobID);
-    dispatch(jobApplyByStudent(jobID));
+  function handleJobApply(jobId) {
+    dispatch(jobApplyByStudent(jobId));
   }
+  if (isLoading) return <></>;
+
   return (
     <UserLayout slateBgStyles={"flex flex-col gap-8 w-2/3 p-8 bg-neutral-50"}>
       {data &&
         data.map((eachCompany) => (
-          <JobCard key={eachCompany.company} company={eachCompany} handleJobApply ={handleJobApply}/>
+          <JobCard
+            key={eachCompany.company}
+            company={eachCompany}
+            handleJobApply={handleJobApply}
+          />
         ))}
     </UserLayout>
   );
