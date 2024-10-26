7ae3852fb013d6e2c25b5068949aedc1a13a1ed5
amartripathi0 <theamartripathi@gmail.com>
2024-10-26 11:27:17 +0530

    fix(UI): password input comp

--- client/src/components/inputField/PasswordInput.jsx
+++ client/src/components/inputField/PasswordInput.jsx
@@ -1,42 +1,68 @@
-import React, { useState } from 'react'
-import {AiFillEyeInvisible , AiFillEye} from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import cn from "../../utils/cn";
 
-const PasswordInput = ({ type , placeholder ,inputFieldStyle,errorMessageStyle ,labelClass, inputFieldContainerStyles , handlePassChange,labelName , label ,ref, validationObj , error}) => {
+const PasswordInput = ({
+  placeholder,
+  inputFieldStyle,
+  errorMessageStyle,
+  labelClass,
+  inputFieldContainerStyles,
+  handlePassChange,
+  labelName,
+  label,
+  ref,
+  validationObj,
+  error,
+}) => {
+  const [showPass, setShowPass] = useState(false);
+  function togglePassHide() {
+    setShowPass((prev) => !prev);
+  }
 
-    const [showPass , setShowPass] = useState(false)
-    function togglePassHide(){
-            setShowPass(prev => !prev)
-    }
   return (
-    <div className={`${inputFieldContainerStyles} flex flex-col gap-1 `}>
-    <label htmlFor= {labelName} className={`${labelClass} text-md font-medium ml-2`}>{label}</label>
-  
-    <div className= {` ${inputFieldStyle ? inputFieldStyle:  "h-12 w-full"} border-black border-2  text-md rounded-md  flex justify-between items-center`}>
-      <input
-      type= {showPass ? "text": "password"}
-      placeholder={placeholder}
-    
-      onChange={handlePassChange}
-      {...validationObj}
-      className='w-11/12 h-full !outline-none pl-3  font-semibold '
+    <div className={cn(inputFieldContainerStyles, "flex flex-col gap-1")}>
+      <label
+        htmlFor={labelName}
+        className={cn(labelClass, "text-sm font-medium ml-2")}
+      >
+        {label}
+      </label>
 
-      />
+      <div
+        className={cn(
+          inputFieldStyle ? inputFieldStyle : "h-10 w-full",
+          " border focus-within:border-neutral-900 border-neutral-500 focus-within:bg-neutral-100 text-sm rounded flex-center"
+        )}
+      >
+        <input
+          type={showPass ? "text" : "password"}
+          placeholder={placeholder}
+          onChange={handlePassChange}
+          {...validationObj}
+          className="w-11/12 h-full outline-none focus:bg-neutral-100 pl-3 rounded font-medium"
+        />
 
-      <button onClick={togglePassHide}>
-        {
-            showPass ? 
-            <AiFillEyeInvisible size = {25} className='mr-2'/>
-            :
-            <AiFillEye size = {25} className = 'mr-2' />
-        }
-      </button>
+        <button onClick={togglePassHide}>
+          {showPass ? (
+            <AiFillEyeInvisible size={25} className="mx-2" />
+          ) : (
+            <AiFillEye size={25} className="mx-2" />
+          )}
+        </button>
+      </div>
 
+      <p
+        className={cn(
+          errorMessageStyle ? errorMessageStyle : "text-red-500 text-xs",
+          " ml-2"
+        )}
+      >
+        {" "}
+        {error}
+      </p>
     </div>
-      
-    <p className={` ${errorMessageStyle ? errorMessageStyle : "h-5 font-medium text-base"} text-red-500  ml-2`}> {error}</p>
-    </div>
+  );
+};
 
-  )
-}
-
-export default PasswordInput
+export default PasswordInput;
