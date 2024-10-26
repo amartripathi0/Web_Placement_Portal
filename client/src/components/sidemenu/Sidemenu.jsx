5d94323edebea383dd28dbd3f596827e7c1dd6a0
amartripathi0 <theamartripathi@gmail.com>
2024-10-26 11:47:32 +0530

    fix: sidemenu text on hover

--- client/src/components/sidemenu/Sidemenu.jsx
+++ client/src/components/sidemenu/Sidemenu.jsx
@@ -118,6 +118,19 @@ export function SidebarItem({ icon: Icon, text }) {
       >
         {text}
       </span>
+      {!expanded && (
+        <div
+          className={`
+            
+            absolute left-full  rounded-md px-2 py-1 ml-6
+            bg-indigo-200 text-indigo-800 text-sm
+            invisible opacity-20 -translate-x-3 transition-all
+            group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+        `}
+        >
+          {text}
+        </div>
+      )}
     </NavLink>
   );
 }
