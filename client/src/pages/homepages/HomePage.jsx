0b61d97a74cb24ca6f20451320abb8a71224257b
amartripathi0 <theamartripathi@gmail.com>
2024-11-09 15:02:12 +0530

    fix: homepage stuck in loading

--- client/src/pages/homepages/HomePage.jsx
+++ client/src/pages/homepages/HomePage.jsx
@@ -3,16 +3,19 @@ import Navbar from "../../components/header/Navbar";
 import { Outlet, useNavigate } from "react-router-dom";
 import { getLoginStatus } from "../../redux/features/common/globalSlice";
 import { useSelector, useDispatch } from "react-redux";
-import LoadingPage from "../LoadingPage";
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { isLoggedin, isSuccess, isLoading, userType } = useSelector(
+  const { isLoggedin, isSuccess, userType } = useSelector(
     (state) => state.globalAuth
   );
   useEffect(() => {
-    if (isLoggedin && isSuccess && userType !== "") {
+    if (
+      isLoggedin &&
+      isSuccess &&
+      userType !== ("visitor" || "")
+    ) {
       navigate(userType);
     } else {
       dispatch(getLoginStatus());
@@ -21,7 +24,6 @@ const HomePage = () => {
 
   return (
     <main>
-      {isLoading && <LoadingPage />}
       <section className={`relative`}>
         <Navbar />
         <Outlet />
